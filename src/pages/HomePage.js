4b699207fac03689902a1c947653373b28596036
W7 <wi7h.solutions@gmail.com>
2024-07-16 11:00:27 -0300

    feat: sections 'O que fazemos?' and 'Time'

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -21,9 +21,19 @@ function prevSlide() {
   showSlide(currentIndex - 1);
 }
 
-// Automatically move to the next slide every 3 seconds
-setInterval(nextSlide, 3000);
+// // Automatically move to the next slide every 3 seconds
+// setInterval(nextSlide, 3000);
 
-var Zaia = {
-  AgentURL: "https://platform.zaia.app/embed/chat/17833"
-};
+// var Zaia = {
+//   AgentURL: "https://platform.zaia.app/embed/chat/17833"
+// };
+
+document.addEventListener('DOMContentLoaded', function () {
+  var progressBars = document.querySelectorAll('.progress-bar');
+
+  progressBars.forEach(function (bar) {
+    var value = bar.getAttribute('data-value');
+    bar.style.width = value + '%';
+    bar.previousElementSibling.querySelector('.darkcolors .progress-percent').textContent = value + '%';
+  });
+});
